5cb4e4e4f0977e7c6a27f6d933197697c22ac97e
Zoe Gonzalez <zusolaris.personal@gmail.com>
2023-01-12 06:35:30 +0000

    completed cc 33 just needs tests

---
+++ hashTable/left-join/hashtable-left-join.js
@@ -0,0 +1,16 @@
+function leftJoin(hash1, hash2) {
+  let result = [];
+  let keys = hash1.keys();
+  if(keys.length === 0) return null;
+  for (const key of keys) {
+    let row = [key, hash1.get(key)];
+    if (hash2.has(key)) {
+      row.push(hash2.get(key));
+    } else {
+      row.push(null);
+    }
+    result.push(row);
+  }
+
+  return result;
+}
